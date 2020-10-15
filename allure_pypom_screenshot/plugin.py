95553a070e03e507f62aba2159772093ad914af0
vishal <vishal.02jan@gmail.co>
2020-10-16 00:10:52 +0530

    fixed allure attachment type attribute

--- allure_pypom_screenshot/plugin.py
+++ allure_pypom_screenshot/plugin.py
@@ -11,7 +11,7 @@ def pypom_after_wait_for_page_to_load(page):
     ''' capture the screenshot of a page once it is loaded '''
     file_name = page.__class__.__name__ + '.png'
     page.selenium.get_screenshot_as_file(file_name)
-    allure.attach.file(file_name, allure.attachment_type.PNG)
+    allure.attach.file(file_name, attachment_type=allure.attachment_type.PNG)
     os.remove(file_name)
 
 
@@ -20,5 +20,5 @@ def pypom_after_wait_for_region_to_load(region):
     ''' capture the screenshot of a region once it is loaded '''
     file_name = region.__class__.__name__ + '.png'
     region.root.screenshot(file_name)
-    allure.attach.file(file_name, allure.attachment_type.PNG)
+    allure.attach.file(file_name, attachment_type=allure.attachment_type.PNG)
     os.remove(file_name)
