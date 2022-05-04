657cc912e3825a54b7717021325b93bc148cdb7f
Vishal Singh Kushwaha <vishal.02jan@gmail.com>
2022-05-04 22:33:06 +0530

    Update plugin.py

--- allure_pypom_screenshot/plugin.py
+++ allure_pypom_screenshot/plugin.py
@@ -9,16 +9,10 @@ from pypom import hookimpl
 @hookimpl
 def pypom_after_wait_for_page_to_load(page):
     ''' capture the screenshot of a page once it is loaded '''
-    file_name = page.__class__.__name__ + '.png'
-    page.driver.get_screenshot_as_file(file_name)
-    allure.attach.file(file_name, attachment_type=allure.attachment_type.PNG)
-    os.remove(file_name)
+    pass
 
 
 @hookimpl
 def pypom_after_wait_for_region_to_load(region):
     ''' capture the screenshot of a region once it is loaded '''
-    file_name = region.__class__.__name__ + '.png'
-    region.root.screenshot(file_name)
-    allure.attach.file(file_name, attachment_type=allure.attachment_type.PNG)
-    os.remove(file_name)
+    pass
